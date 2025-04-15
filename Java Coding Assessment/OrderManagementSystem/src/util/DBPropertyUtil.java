38d2382ce6e65da13776c426acdd1bcd9b1bb0b1
N Lohith Reddy <114926616+NLohithReddy@users.noreply.github.com>
2025-04-15 16:26:44 +0530

    Update DBPropertyUtil.java

--- Java Coding Assessment/OrderManagementSystem/src/util/DBPropertyUtil.java
+++ Java Coding Assessment/OrderManagementSystem/src/util/DBPropertyUtil.java
@@ -6,19 +6,13 @@ import java.util.Properties;
 
 public class DBPropertyUtil {
 
-    public static String getPropertyString(String fileName) throws IOException {
+    public static Properties getProperties(String fileName) {
         Properties props = new Properties();
-        FileInputStream fis = new FileInputStream(fileName);
-        props.load(fis);
-
-        String host = props.getProperty("host");
-        String port = props.getProperty("port");
-        String dbname = props.getProperty("dbname");
-        String username = props.getProperty("username");
-        String password = props.getProperty("password");
-
-        return "jdbc:mysql://" + host + ":" + port + "/" + dbname +
-               "?user=" + username + "&password=" + password +
-               "&useSSL=false&allowPublicKeyRetrieval=true";
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            props.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props;
     }
 }
