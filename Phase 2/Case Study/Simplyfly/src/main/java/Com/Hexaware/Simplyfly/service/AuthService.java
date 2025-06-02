284897386419eb56f0c1b0c41fcb4540854d43f2
N Lohith Reddy <114926616+NLohithReddy@users.noreply.github.com>
2025-06-02 16:57:34 +0530

    Delete Phase 2/Case Study/Simplyfly directory

--- Phase 2/Case Study/Simplyfly/src/main/java/Com/Hexaware/Simplyfly/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package Com.Hexaware.Simplyfly.service;
-
-import Com.Hexaware.Simplyfly.model.User;
-
-public interface AuthService {
-    String login(String email, String password);
-    User findByEmail(String email);
-}
